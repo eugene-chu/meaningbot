9a14b8ced27f57d2768831a9d14b7129f7ff0315
Eugene Chu <eugenchu@gmail.com>
2021-06-17 16:11:46 -0700

    remove node_modules from repo, setting up test commandos, using a testing bot to test and make sure everything is working

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+require('dotenv').config();
+
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
 
 const client = new CommandoClient({
 	commandPrefix: '.',
-	owner: ''
+	owner: ['184136353812840449','294705970511085570','379336215205380106','456947461714345984']
 });
+
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([
+		['first', 'Your First Command Group'],
+		['second', 'Your Second Command Group'],
+	])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+
+  client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+    client.user.setActivity('with Commando');
+  });
+  
+  client.on('error', console.error);
+
+  client.login(process.env.BOTTOKEN);
