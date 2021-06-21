43a0f631978f746e555afce55f312b0c75a889f4
Eugene Chu <eugenchu@gmail.com>
2021-06-20 17:36:13 -0700

    change inhibitor to allow user to use help command in DM

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const { DMChannel } = require('discord.js');
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
 
@@ -9,9 +10,9 @@ const client = new CommandoClient({
 });
 
 client.dispatcher.addInhibitor((message) => {
-  if(message.channel.id !== '848008771397353505')
+  if(message.channel.id !== '848008771397353505' && message.command.name !== 'help')
   return {reason: 'Wrong Channel',
-  response: message.reply('This is the wrong channel. Please use this command in <#848008771397353505>')}});
+  response: message.reply('This is the wrong channel to use the commands. Please use the commands in <#848008771397353505>')}});
 
 client.registry
 	.registerDefaultTypes()
