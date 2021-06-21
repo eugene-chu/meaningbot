bd50131bcd15e68275c563f8ea68fe6e8628ee67
Eugene Chu <eugenchu@gmail.com>
2021-06-20 20:06:37 -0700

    Change/adjust seelog's response

--- commands/first/seelogs.js
+++ commands/first/seelogs.js
@@ -8,13 +8,13 @@ module.exports = class seelogs extends Command{
       group: 'first',
       memberName: 'seelogs',
       description: 'See your current log.',
-      examples: ['`.seelogs`: Current log is \'Hello World\'']
+      examples: ['`.seelogs`: Your current log is\nHello World']
     });
   }
 
   async run(message){
     let isThere = await db.findDoc({'userID': message.author.userId});
     if(!isThere) return await message.reply('You have not added any log yet. Did you want to add one with `.log` command?');
-    return await message.reply(`${isThere.commitLog}`);
+    return await message.reply(`Your current log is:\n${isThere.commitLog}`);
   }
 };
