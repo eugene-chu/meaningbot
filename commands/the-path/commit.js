4774378812690929723e1bb4bb7647f44f84e0a5
Eugene Chu <eugenchu@gmail.com>
2021-06-21 13:11:49 -0700

    Finish commit command: add check to make sure there is none exisiting, add it to the database, add check for error, and add confirmation message

--- commands/the-path/commit.js
+++ commands/the-path/commit.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord.js-commando');
+const db = require('../../db/db');
 
 module.exports = class Commit extends Command{
   constructor(client){
@@ -17,6 +18,11 @@ module.exports = class Commit extends Command{
   }
 
   async run(message, { text }){
-    return await message.direct(`${text} has been committed. Now use \`.remindme\` in #the-path to set the reminder.\nIf need help, type \`.help <command>\` for more details.`);
+    let isThere = await db.findDoc({'userId': message.author.id});
+    if(isThere) return await message.direct('You already have a commitment.\n Did you mean to update it? Use `.update` to update your current commitment.\nYou can see your current commitments with the command `.commitments`.');
+
+    let res = await db.newDoc({'userId': message.author.id, 'commit1': text, 'remindme': 'never'});
+    if(res === null) return await message.direct('There was an error trying to add the commitment. Let Alex or one of the bot master know!');
+    return await message.direct(`${text} has been committed. Now use \`.remindme\` in #the-path to set the reminder.\nIf you need help, type \`.help <command>\` for more details.`);
   }
 };
