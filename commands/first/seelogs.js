e4af0b623ebab7d3aa741b026f5ca62d1ff0ef5e
Eugene Chu <eugenchu@gmail.com>
2021-06-20 20:34:48 -0700

    Fix typo in seelogs. In if check, should only be !isThere, instead of !isThere.commitLog

--- commands/first/seelogs.js
+++ commands/first/seelogs.js
@@ -14,7 +14,7 @@ module.exports = class seelogs extends Command{
 
   async run(message){
     let isThere = await db.findDoc({'userId': message.author.id});
-    if(!isThere.commitLog) return await message.reply('You have not added any log yet. Did you want to add one with `.log` command?');
+    if(!isThere) return await message.reply('You have not added any log yet. Did you want to add one with `.log` command?');
     return await message.reply(`Your current log is:\n${isThere.commitLog}`);
   }
 };
