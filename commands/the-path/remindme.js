7e4719c456d61e6be3e837deccac19e5210910e3
Eugene Chu <eugenchu@gmail.com>
2021-06-21 17:10:53 -0700

    Change some of the examples. Finish writing the code for remindme command

--- commands/the-path/remindme.js
+++ commands/the-path/remindme.js
@@ -9,8 +9,8 @@ module.exports = class RemindMe extends Command {
       memberName: 'remindme',
       description: 'Set a reminder interval. Meaningbot will DM you, reminding you to get after it!',
       examples: ['`.remindme daily`: Gets a DM daily to remind you to get after it!',
-                 '`.remindme weekly`: Get a DM every week to remind you to get after it!',
-                 '`.remindme never`: Stop getting DM reminders. However, Meaningbot will still keep your commitment logs. So you can start again at any time.'],
+                 '`.remindme weekly`: Get a DM weekly to remind you to get after it!',
+                 '`.remindme never`: Stop getting DM reminders. But Meaningbot will still have your current commitments.'],
       args: [{
         key: 'frequency',
         prompt: 'How often do you want to be reminded?',
@@ -21,8 +21,13 @@ module.exports = class RemindMe extends Command {
 
   async run(message, { frequency }){
     let isThere = await db.findUser({'userId': message.author.id});
-    if(!isThere) return await message.direct('You have not created a commitment yet.\nUser `.commit` to add your first commitment!');
+    if(!isThere) return await message.direct('You have not created a commitment yet.\nUse `.commit` to add your first commitment!');
 
     let res = await db.updateReminder({'id': message.author.id, 'reminder': frequency});
+    if(res === null) return await message.direct('There was an error trying to update the commitment. Let Alex or one of the bot masters know!');
+    let resMessage;
+    frequency === 'never' ? resMessage = `You will stop getting DM reminders. However, Meaningbot will still keep your commitments logged. Start is up again with \`.remindme\``
+                          : resMessage = `You will get ${frequency} DM reminding you to get after it!`
+    return await message.direct(resMessage);
   }
 }
