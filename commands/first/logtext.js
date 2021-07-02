58c39c732a39e49b00186395b7dffe41a037d8ca
Eugene Chu <eugenchu@gmail.com>
2021-07-02 15:49:52 -0700

    Playing around with client's 'presenceUpdate' event listener and setInterval method. Also trying to set a global Set list to keep track of user IDs that can be accessed in other commands

--- commands/first/logtext.js
+++ commands/first/logtext.js
@@ -1,8 +1,10 @@
 const { Command } = require('discord.js-commando');
 const db = require('../../db/db.js');
 
+const { userList } = require('../../index.js');
+
 module.exports = class log extends Command {
-  constructor(client){
+  constructor(client) {
     super(client, {
       name: 'log',
       group: 'first',
@@ -13,15 +15,16 @@ module.exports = class log extends Command {
         key: 'text',
         prompt: 'What will be your commitment?',
         type: 'string',
-      }]
+      }],
     });
   }
 
-  async run(message, { text }){
-    let isThere = await db.findDoc({'userId': message.author.id});
-    if(isThere) return await message.direct('You already have a commitment. \nDid you mean to update it? Use \`.update\` to update it. \nOr you can use \`.seelogs\` to see your current log.');
+  async run(message, { text }) {
+    const isThere = await db.findDoc({ 'userId': message.author.id });
+    if(isThere) return await message.direct('You already have a commitment. \nDid you mean to update it? Use `.update` to update it. \nOr you can use `.seelogs` to see your current log.');
 
-    let res = await db.newDoc({'userId': message.author.id, 'commitLog': text, 'remindme': 'never'});
+    userList.add(message.author.id);
+    const res = await db.newDoc({ 'userId': message.author.id, 'commitLog': text, 'remindme': 'never' });
     if(res === null) return await message.direct('There was an error trying to add the log');
 
     return await message.direct(`${text} has been logged. Now use \`.remindme daily/weekly/bi-weekly/monthly\` to set the reminder.`);
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 require('dotenv').config();
 
-const { DMChannel } = require('discord.js');
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
 
+const userList = new Set();
+
 const client = new CommandoClient({
 	commandPrefix: '.',
-	owner: ['184136353812840449','294705970511085570','379336215205380106','456947461714345984']
+	owner: ['184136353812840449', '294705970511085570', '379336215205380106', '456947461714345984'],
 });
 
 client.dispatcher.addInhibitor((message) => {
-  if(!(message.command.name === 'help' && message.channel.type === 'dm')){
-    if(message.channel.id !== '848008771397353505'){
-        return {reason: 'Wrong Channel',
-        response: message.reply('This is the wrong channel to use the commands. Please use the commands in <#848008771397353505>')}
-    }
+	if(!(message.command.name === 'help' && message.channel.type === 'dm')) {
+		if(message.channel.id !== '848008771397353505') {
+			return { reason: 'Wrong Channel',
+				response: message.reply('This is the wrong channel to use the commands. Please use the commands in <#848008771397353505>') };
+		}
 
-  }
+	}
 });
 
 client.registry
@@ -24,17 +25,31 @@ client.registry
 	.registerGroups([
 		['first', 'Your First Command Group'],
 		['second', 'Your Second Command Group'],
-    ['misc', 'misc but important commands']
+		['misc', 'misc but important commands'],
 	])
 	.registerDefaultGroups()
 	.registerDefaultCommands()
 	.registerCommandsIn(path.join(__dirname, 'commands'));
 
-  client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-    client.user.setActivity('Playing Around');
-  });
-  
-  client.on('error', console.error);
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+	client.user.setActivity('Playing Around');
+});
 
-  client.login(process.env.BOTTOKEN);
+client.on('error', console.error);
+
+client.on('presenceUpdate', (pres1, pres2) => {
+  console.log(pres1.userID);
+	console.log(`arg1: ${JSON.stringify(pres1)}`);
+	console.log(`agr2: ${JSON.stringify(pres2)}`);
+});
+
+client.setInterval(() => {
+  console.log(`This is the current userList: ${JSON.stringify(userList)}`);
+}, 10 * 1000);
+
+client.login(process.env.BOTTOKEN);
+
+module.exports = {
+  userList,
+};
