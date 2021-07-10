f7a219c0c464407ac50b8bd968e221c0e11a10b6
Eugene Chu <eugenchu@gmail.com>
2021-07-10 16:54:47 -0700

    Remove the now variable, create a new Date object and pass it into the addcommit function to store a date object, not a string, into the database.

--- commands/the-path/commit.js
+++ commands/the-path/commit.js
@@ -21,9 +21,7 @@ module.exports = class Commit extends Command{
     let isThere = await db.findUser(message.author.id);
     if(isThere) return await message.direct('You already have a commitment.\nDid you mean to update it? Use `.update` to update your current commitment.\nYou can see your current commitments with the command `.commitments`.');
 
-    let now = Date();
-    
-    let res = await db.newCommit(message.author.id, text, now);
+    let res = await db.newCommit(message.author.id, text, new Date());
     if(res === null) return await message.direct('There was an error trying to add the commitment. Let Alex or one of the bot master know!');
     return await message.direct(`${text} has been committed. Now use \`.remindme\` in #the-path to set the reminder.\nIf you need help, type \`.help <command>\` for more details.`);
   }
