13d79ec86758e9f57f0bb55f134ec754d6793e38
Eugene Chu <eugenchu@gmail.com>
2021-07-10 17:47:18 -0700

    Add a commend to describe the shape of a commit log in the database. Add status param and a status field to newCommmit method. Add a updateStatus method to update the status field whenever the user changes status. Fix styling/syntax and minor typos

--- db/db.js
+++ db/db.js
@@ -14,22 +14,33 @@ client.connect(() => {
   console.log('successfully connected to the database')
 });
 
+/**
+ * Each collection of commitments will include the following:
+ * userId: The user's discord uuid. For easy search
+ * commit: The commitment message the user intend to have the bot keep and send reminder DM of
+ * remindMe: The frequency on how often the user sends the reminder DMs. Can either be 'never', 'daily', 'weekly', or 'monthly'
+ * commitTime: The time when the bot received the first .commit command or .remindMe command.
+ * lastDMTime: The last time the bot DM messaged the user
+ * status: The last known status of the user. Either 'offline' or 'online' ('online' includes 'dnd' and 'idle');
+ */
+
 module.exports = {
   // Look of the discord user's unqiue id in the database.
   findUser: async function(id) {
     try{
       return await col.findOne({'userId': id})
     } catch (err){
-      console.log(err);
+      console.error(err);
       return null;
     }
   },
   // Add a new commitment to the database.
-  newCommit: async function(id, commit, time) {
+  newCommit: async function(id, commit, time, status) {
     /** Param should be the following:
      *    userId: <user id, using their discord's unique id>
      *    commit: <commitment message>
      *    time: <the time (JS Date object) when the commit command was run>
+     *    status: <The user's status when the commitment is created>
      * 
      * Working on allowing for more than 1 commit message
      */
@@ -39,9 +50,10 @@ module.exports = {
         'commit': commit,
         'remindMe': 'never',
         'commitTime': time,
-        'lastDMTime': time });
+        'lastDMTime': time,
+        'status': status });
     } catch (err){
-      console.log(err);
+      console.error(err);
       return null;
     }
   },
@@ -56,7 +68,7 @@ module.exports = {
       return await col.updateOne({'userId': id},
       { $set: { 'commit': commit } });
     } catch (err){
-      console.log(err);
+      console.error(err);
       return null;
     }
   },
@@ -69,9 +81,9 @@ module.exports = {
      */
     try{
         return await col.updateOne({ 'userId': id },
-        { $set: { 'remindMe': frequency, 'commitTime': time, 'lastDMTime': time }})
+        { $set: { 'remindMe': frequency, 'commitTime': time, 'lastDMTime': time }});
     } catch (err){
-      console.log(err);
+      console.error(err);
       return null;
     }
   },
@@ -82,9 +94,18 @@ module.exports = {
      */
     try{
       return await col.updateOne({ 'userId': id },
-      { $set: { 'lastDMTime': time }})
-    }catch (err){
-      console.log(err);
+      { $set: { 'lastDMTime': time }});
+    } catch (err){
+      console.error(err);
+      return null;
+    }
+  },
+  updateStatus: async function(id, status){
+    try{
+      return await col.updateOne({ 'userId': id },
+      { $set: { 'status': status } });
+    } catch (err) {
+      console.error(err);
       return null;
     }
   }
