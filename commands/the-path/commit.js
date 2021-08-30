de7a43e83ea550fd90cc964afdb469f94bb2d49a
Eugene Chu <eugenchu@gmail.com>
2021-08-30 14:11:20 -0700

    Made all custom emotes be display properly. Also added a few new lines to getsome command

--- commands/the-path/commit.js
+++ commands/the-path/commit.js
@@ -17,23 +17,23 @@ module.exports = class Commit extends Command{
     });
 
     this.quotesList = [
-      'Noted! Now get after it! :getafterit:',
-      'Thanks, and good luck on The Path! :getafterit:',
-      'Commitments are the first step, stick to The Path! :getafterit:',
-      'You bad motherfuckers! :akira_happy:',
-      'When you do a little more than you can you get stronger, and we don\'t know the upper limit to that. :jbp6:',
-      'Action is the next step to realize your goals. :akira_happy:',
-      'Get creative. Get aggressive. Get it done! When you are on the road, STAY ON THE PATH',
-      'Fear is normal. Every person feels fear at some point. Step aggressively toward to your fear - that is the step into bravery :getafterit:',
-      'You have to do the work. You have to hold the line. You have to make it happen :getafterit:',
-      'Don\'t count on motivation. Count on Discipline :getafterit:',
-      'Set your alarm clock and get out of bed when it goes off :getafterit:',
-      'Go down swinging. And I\'ll tell you: If you fight with all you have, more often than not, you won\'t go down at all. You will win. :getafterit:',
-      'Discipline Equals Freedom :getafterit:',
-      'It\'ll destroy you if you try to make it mean anything to anyone but yourself. :jbp6: ',
-      'The average is the borderline that keeps mere men in their place. Those who step over the line are heroes by the very act. Go! :jbp1:',
-      'Scar tissue is stronger than regular tissue. Realize the strength, move on. :getafterit:',
-      'Begin - to begin is half the work, let half still remain; again begin this, and you will have finished. :getafterit:'
+      'Noted! Now get after it! <:getafterit:479505436282847257>479505436282847257>',
+      'Thanks, and good luck on The Path! <:getafterit:479505436282847257>479505436282847257>',
+      'Commitments are the first step, stick to The Path! <:getafterit:479505436282847257>479505436282847257>',
+      'You bad motherfuckers! <:akira_happy:465667255087136799>',
+      'When you do a little more than you can you get stronger, and we don\'t know the upper limit to that. <:jbp6:460235086986608640>',
+      'Action is the next step to realize your goals. <:akira_happy:465667255087136799>',
+      'Get creative. Get aggressive. Get it done! When you are on the road, STAY ON THE PATH <:getafterit:479505436282847257>479505436282847257>',
+      'Fear is normal. Every person feels fear at some point. Step aggressively toward to your fear - that is the step into bravery <:getafterit:479505436282847257>',
+      'You have to do the work. You have to hold the line. You have to make it happen <:getafterit:479505436282847257>',
+      'Don\'t count on motivation. Count on Discipline <:getafterit:479505436282847257>',
+      'Set your alarm clock and get out of bed when it goes off <:getafterit:479505436282847257>',
+      'Go down swinging. And I\'ll tell you: If you fight with all you have, more often than not, you won\'t go down at all. You will win. <:getafterit:479505436282847257>',
+      'Discipline Equals Freedom <:getafterit:479505436282847257>',
+      'It\'ll destroy you if you try to make it mean anything to anyone but yourself. <:jbp6:460235086986608640>  ',
+      'The average is the borderline that keeps mere men in their place. Those who step over the line are heroes by the very act. Go! <:jbp1:460235087192129539>  ',
+      'Scar tissue is stronger than regular tissue. Realize the strength, move on. <:getafterit:479505436282847257>',
+      'Begin - to begin is half the work, let half still remain; again begin this, and you will have finished. <:getafterit:479505436282847257>'
     ];
   }
 
