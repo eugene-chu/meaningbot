2352ddeaf1aeb549e242499cd3f2279778101ca2
Eugene Chu <eugenchu@gmail.com>
2021-09-04 15:58:02 -0700

    Removing the DM reminder message logic from update command. From now on, update will only send an encouraging message back to the user. The DM reminder update message is done in the commit command

--- commands/the-path/update.js
+++ commands/the-path/update.js
@@ -40,11 +40,6 @@ module.exports = class Update extends Command {
   async run(message, { text }){
     let randQuote = this.quotesList[Math.floor(Math.random()*this.quotesList.length)];
 
-    let isThere = await db.findUser(message.author.id);
-    if(!isThere) return await message.reply('You have not created a commitment yet.\nUse `!commit` to add your first commitment!');
-
-    let res = await db.updateCommit(message.author.id, text, new Date());
-    if(res === null) return await message.reply('There was an error trying to update the commitment. Let Alex or one of the bot masters know!');
-    return await message.reply(`${randQuote} \n Your commitment has been updated! Commitment changed from \n"${isThere.commit}"\nto\n"${text}"`);
+    return await message.reply(randQuote);
   }
 };
